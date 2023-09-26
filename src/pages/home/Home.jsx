062d2a4a270b107c0585d801354284f203b5408d
dhiflaouiamira <dhiflaouiamira98@gmail.com>
2023-09-26 19:41:30 +0200

    add hotels page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./home.css"
+import Navbar from '../../components/navbar/Navbar'
+import Header from '../../components/header/Header'
+
+
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
+    <div>
+    <Navbar/>
+    <Header/>
+    <div className="homecontainer"></div>
+    </div>
+
+    )
 }
 
 export default Home
